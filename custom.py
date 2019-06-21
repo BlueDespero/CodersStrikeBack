4d20b5c40adeb459ae05de35378826b43b73abf6
BlueDespero <cezarytroska@gmail.com>
2019-06-21 07:25:16 +0200

    Main code

---
+++ custom.py
@@ -0,0 +1,9 @@
+tools = {
+    "board_size_x" : 2400,
+    "board_size_y" : 1600,
+    "checkpoint_radius" : 30,
+    "hm_checkpoints" : 4,
+    "hm_pods":2,
+    "Teams":1,
+    "pod_radius" : 20
+}
